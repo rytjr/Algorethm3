3b4c5982ec17a160c7851d2ab7f8d7881a83a72e
rytjr <kks007159@naver.com>
2024-06-01 20:58:11 +0900

    time complexity

---
+++ src/Algorethm3/Algorethm3.java
@@ -0,0 +1,15 @@
+package Algorethm3;
+
+import java.util.*;
+
+public class Algorethm3 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long num = sc.nextInt();
+		
+		System.out.println(num*num);
+		System.out.print(2);
+	}
+}
